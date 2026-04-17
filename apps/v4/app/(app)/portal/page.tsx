162c730f07effb14549d27ec8ce12b3908697217
snow-crystal-220 <snow220crystal@outlook.com>
2026-04-17 07:58:26 -0700

    page.tsx

---
+++ apps/v4/app/(app)/portal/page.tsx
@@ -0,0 +1,40 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/registry/new-york-v4/ui/sidebar"
+import { AppSidebar } from "@/app/(app)/portal/components/app-sidebar"
+import { ChartAreaInteractive } from "@/app/(app)/portal/components/chart-area-interactive"
+import { DataTable } from "@/app/(app)/portal/components/data-table"
+import { SectionCards } from "@/app/(app)/portal/components/section-cards"
+import { SiteHeader } from "@/app/(app)/portal/components/site-header"
+
+import data from "./data.json"
+
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 64)",
+          "--header-height": "calc(var(--spacing) * 12 + 1px)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="sidebar" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
